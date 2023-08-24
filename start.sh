243686a00a8c44cae18ded7a66119b1af2e653ec
hoka4882@gmail.com <hoka4882@gmail.com>
2023-08-24 12:46:17 +0100

    supression battle

--- start.sh
+++ start.sh
@@ -4,8 +4,11 @@ if [[ $EUID -ne 0 ]]; then
    	echo "This script must be run as root" 
    	exit 1
 else
+	#Suppress "Daemons using outdated libraries" with an auto restart daemon configuration
+	sed "/#\$nrconf{restart} = 'i';/s/.*/\$nrconf{restart} = 'a';/" /etc/needrestart/needrestart.conf
+
     #Update and Upgrade
     echo "Updating and Upgrading"
-    apt-get -yqq update && sudo apt-get upgrade -yqq
+    apt-get -yqq update && sudo apt-get upgrade -yqq > apt-get.log
     #vscode
 fi
