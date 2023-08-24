a541cc953369cb3c82f277ac1174ee0aa45188aa
hoka4882@gmail.com <hoka4882@gmail.com>
2023-08-24 12:51:48 +0100

    -i

--- start.sh
+++ start.sh
@@ -4,8 +4,8 @@ if [[ $EUID -ne 0 ]]; then
    	echo "This script must be run as root" 
    	exit 1
 else
-	#Suppress "Daemons using outdated libraries" with an auto restart daemon configuration
-	sed "/#\$nrconf{restart} = 'i';/s/.*/\$nrconf{restart} = 'a';/" /etc/needrestart/needrestart.conf
+    #Suppress "Daemons using outdated libraries" with an auto restart daemon configuration
+    sed -i "/#\$nrconf{restart} = 'i';/s/.*/\$nrconf{restart} = 'a';/" /etc/needrestart/needrestart.conf
 
     #Update and Upgrade
     echo "Updating and Upgrading"
